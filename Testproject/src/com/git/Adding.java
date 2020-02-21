b2e211200c28a927c138a586b35c4bf48a65b5c7
HP <HP@DESKTOP-2RJT2SC>
2020-02-21 13:44:20 +0530

    adding numbers

---
+++ Testproject/src/com/git/Adding.java
@@ -0,0 +1,14 @@
+package com.git;
+
+public class Adding {
+
+	public static void main(String[] args) {
+		int i=1;
+		int s=2;
+		int z=i+s;
+		System.out.println(s);
+		System.out.println("welcome");
+
+	}
+
+}
